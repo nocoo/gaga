70d4e82ce131faaa55eb38cecfba8f3981996bfb
Zheng Li <lizheng@lizheng.me>
2026-09-11 22:01:09 +0800

    fix: serve /api/live as json

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ function apiLivePlugin(): Plugin {
       });
     },
     generateBundle() {
-      this.emitFile({ type: 'asset', fileName: 'api/live', source: body });
+      this.emitFile({ type: 'asset', fileName: 'api/live.json', source: body });
     },
   };
 }
